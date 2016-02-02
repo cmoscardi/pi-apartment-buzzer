adb7be530fb81f679141a47f1679f5a704419727
Christian Moscardi <moscardi79@gmail.com>
2016-02-02 02:51:15 +0000

    an endpoint to check that the app works, correctly formatted response

--- app.py
+++ app.py
@@ -2,16 +2,25 @@ from flask import Flask, request
 
 from buzz import buzz
 
+RESP = """<?xml version="1.0" encoding="UTF-8" ?>  
+<Response> 
+</Response>"""
+
 def get_app():
     app = Flask(__name__)
 
     @app.route("/send_text", methods=["POST"])
     def send_text():
-        buzz(request.get_json()['Body'].strip())
-        return "OK"
+        buzz(request.form['Body'].strip())
+        return RESP
     
+
+    @app.route("/")
+    def index():
+        return "HEY"
+
     return app
 
 if __name__ == "__main__":
     app = get_app()
-    app.run(debug=True)
+    app.run(debug=True, port=80, host='0.0.0.0')
